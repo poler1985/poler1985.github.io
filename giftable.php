c1bf581c87ba1acddc8ceef8ebb286fdb450ad67
poler1985 <olgayakovleva@Olgas-MacBook-Pro.local>
2014-11-02 17:38:25 -0500

    added PHP file

---
+++ giftable.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form submission</title>
+    </head>
+
+    <body>
+        <form action="demo.php" method="post">
+            First Name: <input type="text" name="first_name"><br>
+            Last Name: <input type="text" name="last_name"><br>
+            Email: <input type="text" name="email"><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+
+    </body>
+</html> 
